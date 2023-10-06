96c95789b8836fdb646a3416f89652b0821bdd4c
admcleanplastic <139787299+admcleanplastic@users.noreply.github.com>
2023-10-06 10:54:37 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import sqlite3
 import streamlit as st
+from datetime import date
 
 # Função para remover um usuário da tabela
 def remove_user(user_id):
@@ -18,11 +19,21 @@ def list_users():
     conn.close()
     return users
 
-# Função para editar um usuário na tabela
-def edit_user(user_id, new_user, new_password, new_nome, new_email):
+# Função para adicionar um usuário à tabela
+def add_user(data, empresa, nome_completo, usuario, turno, setor, senha, equipamento):
     conn = sqlite3.connect('novo.db')
     cursor = conn.cursor()
-    cursor.execute('UPDATE operador SET user = ?, senha = ?, nome = ?, email = ? WHERE ID = ?', (new_user, new_password, new_nome, new_email, user_id))
+    cursor.execute('INSERT INTO operador (data, empresa, nome_completo, user, turno, setor, senha, equipamento) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                   (data, empresa, nome_completo, usuario, turno, setor, senha, equipamento))
+    conn.commit()
+    conn.close()
+
+# Função para editar um usuário na tabela
+def edit_user(user_id, data, empresa, nome_completo, usuario, turno, setor, senha, equipamento):
+    conn = sqlite3.connect('novo.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE operador SET data = ?, empresa = ?, nome_completo = ?, user = ?, turno = ?, setor = ?, senha = ?, equipamento = ? WHERE ID = ?',
+                   (data, empresa, nome_completo, usuario, turno, setor, senha, equipamento, user_id))
     conn.commit()
     conn.close()
 
@@ -34,14 +45,19 @@ st.write('Editar Usuário')
 users = list_users()
 selected_user = st.selectbox('Selecione um usuário para editar:', users)
 if selected_user:
-    user_id, old_user, old_password, old_nome, old_email = selected_user
-    new_user = st.text_input('Novo nome de usuário:', old_user)
-    new_password = st.text_input('Nova senha:', old_password, type='password')
-    new_nome = st.text_input('Novo nome:', old_nome)
-    new_email = st.text_input('Novo email:', old_email)
+    user_id, data, empresa, nome_completo, usuario, turno, setor, _, _ = selected_user
+    new_data = st.date_input("Data", value=data, key="data")
+    new_empresa = st.selectbox('Empresa', ['Clean Plastic'], index=0 if empresa == 'Clean Plastic' else 1)
+    new_nome_completo = st.text_input("Nome Completo:", value=nome_completo).upper()
+    new_usuario = st.text_input("Usuário", value=usuario).upper()
+    new_turno = st.selectbox('Turno:', ['1º Turno', '2º Turno', '3º Turno', 'ADM'], index=['1º Turno', '2º Turno', '3º Turno', 'ADM'].index(turno))
+    new_setor = st.selectbox('Setor:', ['Blenda', 'Recebimento', 'Logística'], index=['Blenda', 'Recebimento', 'Logística'].index(setor))
+    new_senha = st.text_input('Senha', value="", type='password')
+    new_equipamento = st.selectbox('Equipamento:', ['Empilhadeira Recebimento 01'], index=0)
+
     if st.button('Editar'):
-        edit_user(user_id, new_user, new_password, new_nome, new_email)
-        st.success(f'Usuário "{old_user}" editado com sucesso!')
+        edit_user(user_id, new_data, new_empresa, new_nome_completo, new_usuario, new_turno, new_setor, new_senha, new_equipamento)
+        st.success(f'Usuário "{nome_completo}" editado com sucesso!')
 
 # Formulário para remover um usuário
 st.write('Remover Usuário')
@@ -50,7 +66,7 @@ if selected_user:
     user_id = selected_user[0]
     if st.button('Remover'):
         remove_user(user_id)
-        st.success(f'Usuário "{selected_user[1]}" removido com sucesso!')
+        st.success(f'Usuário "{selected_user[3]}" removido com sucesso!')
 
 # Lista de usuários existentes
 st.write('Usuários Existentes')
