412c61c31de083ec2d2dac4774165a31e75f3d68
admcleanplastic <139787299+admcleanplastic@users.noreply.github.com>
2023-10-06 14:48:10 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import sqlite3
 import streamlit as st
-import pandas as pd
 
 st.title("Gerenciar Entradas")
 
@@ -14,48 +13,49 @@ def listar_registros():
     registros = cursor.fetchall()
     return registros
 
-# Carregar os registros em um DataFrame Pandas
-registros = listar_registros()
-df = pd.DataFrame(registros, columns=["ID", "Data", "Local Entrada", "Estado", "Cidade", "Razão Social", "Motivo", "Tipo Veículo", "Frete Retorno", "Status Veículo", "Placa", "Nome Completo", "Telefone", "Info Complementar", "Status", "Número Page"])
+# Função para editar um registro
+def editar_registro(registro):
+    data, local_entrada, estado, cidade, razao_social, motivo, tipo_veiculo, frete_retorno, status_veiculo, placa, nome_completo, telefone, info_complementar, status, numero_page, id = registro
 
-# Exibir os registros em uma tabela Pandas
-st.write(df)
+    # Exibir os campos na interface para edição
+    data = st.date_input("Data Entrada", value=data)
+    local_entrada = st.selectbox('Local de entrada', ['Clean Plastic', 'Clean Poa', 'Clean Jundiai', 'Clean Bottle', 'Clean Fortal', 'Raposo Plasticos', 'Raposo Minas', 'Fornecedor PF', 'Outro'], index=local_entrada)
+    estado = st.selectbox('Selecione o estado', estados, index=estados.index(estado))
+    cidade = st.selectbox('Selecione a cidade', municipios, index=municipios.index(cidade))
+    razao_social = st.text_input('Razão Social', razao_social)
+    motivo = st.selectbox('Motivo:', ['Carregar', 'Descarregar', 'Entrega', 'Retirada'], index=motivo)
+    tipo_veiculo = st.selectbox('Tipo de Veiculo:', ["Truck-Side", "Carreta-Side", "Truck-Grade Baixa", "Carreta-Grade Baixa", "Carreta Graneleira", "Container","Bitrem","Bitruck"], index=tipo_veiculo)
+    placa = st.text_input('Placa do Veiculo', placa).upper()
+    nome_completo = st.text_input("Nome Completo", nome_completo).upper()
+    telefone = st.text_input('Telefone', telefone).upper()
+    frete_retorno = st.selectbox('Possuem Frete Retorno?', ['Sim', 'Nao'], index=frete_retorno)
+    numero_page = st.number_input('Número do Page', min_value=1, max_value=16, step=1, value=numero_page)
+    info_complementar = st.text_area('Info. Complementar', info_complementar)
 
-# Opções para editar e excluir registros
-opcao = st.selectbox("Selecione uma opção:", ["Editar", "Excluir"])
+    # Botão para atualizar o registro
+    if st.button("Atualizar"):
+        cursor.execute('''UPDATE entrada SET data=?, local_entrada=?, estado=?, cidade=?, razao_social=?, motivo=?, tipo_veiculo=?, frete_retorno=?, status_veiculo=?, placa=?, nome_completo=?, telefone=?, info_complementar=?, numero_page=? WHERE id=?''',
+                       (data, local_entrada, estado, cidade, razao_social, motivo, tipo_veiculo, frete_retorno, status_veiculo, placa, nome_completo, telefone, info_complementar, numero_page, id))
+        conn.commit()
+        st.success("Registro atualizado com sucesso!")
 
-if opcao == "Editar":
-    st.subheader("Editar Registro")
-    id_registro = st.number_input("ID do Registro para Edição", min_value=1, max_value=len(df))
-
-    if st.button("Carregar Registro"):
-        registro_selecionado = df.iloc[id_registro - 1]
-        st.write("Registro Selecionado:")
-        st.write(registro_selecionado)
-
-        # Interface para editar os campos
-        novo_valor = st.text_input("Novo Valor")
-        coluna_para_editar = st.selectbox("Selecione a Coluna para Editar", df.columns)
-        
-        if st.button("Aplicar Edição"):
-            df.at[id_registro - 1, coluna_para_editar] = novo_valor
-            st.success("Edição aplicada com sucesso!")
-
-            # Atualizar o registro no banco de dados
-            cursor.execute(f"UPDATE entrada SET {coluna_para_editar} = ? WHERE ID = ?", (novo_valor, id_registro))
-            conn.commit()
-
-elif opcao == "Excluir":
-    st.subheader("Excluir Registro")
-    id_registro = st.number_input("ID do Registro para Excluir", min_value=1, max_value=len(df))
-
-    if st.button("Excluir Registro"):
-        df.drop(index=id_registro - 1, inplace=True)
+    # Botão para excluir o registro
+    if st.button("Excluir"):
+        cursor.execute("DELETE FROM entrada WHERE id=?", (id,))
+        conn.commit()
         st.warning("Registro excluído com sucesso!")
 
-        # Excluir o registro do banco de dados
-        cursor.execute("DELETE FROM entrada WHERE ID = ?", (id_registro,))
-        conn.commit()
+# Listar todos os registros
+registros = listar_registros()
+
+# Exibir os registros na interface
+if registros:
+    for registro in registros:
+        st.subheader(f"Registro #{registro[0]}")
+        editar_registro(registro)
+else:
+    st.info("Nenhum registro encontrado na tabela 'entrada'.")
 
 # Fechar a conexão com o banco de dados
 conn.close()
+
