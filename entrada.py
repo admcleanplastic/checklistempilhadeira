a72f6f099ed3bf67be08a6e070a169ac56ae7871
admcleanplastic <139787299+admcleanplastic@users.noreply.github.com>
2023-10-06 09:59:24 -0300

    Update entrada.py

--- entrada.py
+++ entrada.py
@@ -1,5 +1,4 @@
 import sqlite3
-import csv
 import streamlit as st
 from datetime import date
 
@@ -14,17 +13,17 @@ col1, col2 = st.columns(2)
 with col1:
     Nome_completo = st.text_input("Nome Completo:").upper()
     user = st.text_input("Usuário").upper()
-    Turno = st.selectbox('Turno:', ['1º Turno','2º Turno','3º Turno','ADM'])
+    Turno = st.selectbox('Turno:', ['1º Turno', '2º Turno', '3º Turno', 'ADM'])
 
 with col2:
-    Setor = st.selectbox('Setor:', ['Blenda','Recebimento','Logística'])
+    Setor = st.selectbox('Setor:', ['Blenda', 'Recebimento', 'Logística'])
     password = st.text_input('Senha', type='password')
     Equipamento = st.selectbox('Equipamento:', ['Empilhadeira Recevimento 01'])
 
-def is_form_valid(data, nome_completo, user, Turno, Setor, password, Equipamento):
+def is_form_valid(data, Nome_completo, user, Turno, Setor, password, Equipamento):
     required_fields = [
         data,
-        nome_completo,
+        Nome_completo,
         user,
         Turno,
         Setor,
@@ -39,7 +38,7 @@ def is_form_valid(data, nome_completo, user, Turno, Setor, password, Equipamento
     return True
 
 if st.button("Salvar"):
-    if is_form_valid(data, nome_completo, user, Turno, Setor, password, Equipamento):
+    if is_form_valid(data, Nome_completo, user, Turno, Setor, password, Equipamento):
         # Conectar ao banco de dados ou criar um novo se ele não existir
         conn = sqlite3.connect('novo.db')
 
@@ -50,7 +49,7 @@ if st.button("Salvar"):
         cursor.execute('''CREATE TABLE IF NOT EXISTS operador (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         data TEXT,
-                        nome_completo TEXT,
+                        Nome_completo TEXT,
                         user TEXT,
                         Turno TEXT,
                         Setor TEXT,
@@ -60,7 +59,7 @@ if st.button("Salvar"):
 
         # Obter os valores dos campos
         data_value = data.strftime("%Y-%m-%d")
-        nome_completo_value = nome_completo
+        Nome_completo_value = Nome_completo
         user_value = user
         Turno_value = Turno
         Setor_value = Setor
@@ -70,7 +69,7 @@ if st.button("Salvar"):
         # Inserir os valores na tabela 'operador'
         cursor.execute('''INSERT INTO operador (
                         data,
-                        nome_completo,
+                        Nome_completo,
                         user,
                         Turno,
                         Setor,
@@ -80,7 +79,7 @@ if st.button("Salvar"):
                     VALUES (?, ?, ?, ?, ?, ?, ?)''',
                    (
                        data_value,
-                       nome_completo_value,
+                       Nome_completo_value,
                        user_value,
                        Turno_value,
                        Setor_value,
@@ -94,3 +93,4 @@ if st.button("Salvar"):
         st.success("Dados salvos com sucesso!")
     else:
         st.warning("Preencha todos os campos antes de salvar.")
+
